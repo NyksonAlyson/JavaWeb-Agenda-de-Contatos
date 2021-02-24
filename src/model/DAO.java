4611a36da3ec765c408d023a72513d7674bd607e
nyksonalyson <nyksonalison@hotmail.com>
2021-02-24 16:02:21 -0300

    Conexao com MySQL - Camada MODEL

--- src/model/DAO.java
+++ src/model/DAO.java
@@ -1,5 +1,38 @@
 package model;
 
-public class DAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DAO {
+	// Módulo de conexão
+	//parametro de conexao
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://127.0.0.1:3306/dbagenda?useTimezone=true&serverTimezone=UTC";
+	
+	private String user = "root";
+	private String password = "@Bboy1504";
+	
+	private Connection conectar() {
+		Connection con = null;
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, user, password);
+			return con;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+
+		}
+	}
+	// teste de conexão
+	public void testeConexao() {
+		try {
+			Connection con = conectar();
+			System.out.println(con);
+			con.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e);
+		}
+	}
 }
